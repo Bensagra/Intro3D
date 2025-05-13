def9236ccb4d5fc1915e3a7455053b6eff62455b
milenaparysow <163904110+milenaparysow@users.noreply.github.com>
2025-05-13 12:07:51 -0300

    dsadsa

---
+++ Assets/Scripts/Instantiator.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject cuboPrefab; // Reference to the prefab to instantiate
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+public void InstanciarCubo(){
+    Instantiate(cuboPrefab);
+}
+}
